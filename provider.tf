5354ccc5bed0d7d73179a0682d2c74442e73107c
afaulkner <avfaulkner@gmail.com>
2021-06-16 20:10:41 -0700

    add backend config

--- provider.tf
+++ provider.tf
@@ -12,9 +12,19 @@ terraform {
       source = "hashicorp/tls"
     }
   }
+  backend "azurerm" { # Create remote backend to store terraform state file
+    storage_account_name = "tfstate2026"
+    container_name       = "tfstate" # The name of the blob container.
+    key                  = "terraform.tfstate" #The name of the state store file to be created.
+    # subscription_id      = "343d0214-92ab-4fd5-9cfd-55b9faba1e06"
+    # tenant_id            = "715c7afb-027a-4b6e-9a60-dc385a62cf18"
+    # access_key = "vPzdW71ACXJZYLZNRP4dsFtDTPxMAqfHGjbAOFm1FaDmLobNEckGHCN8xRaxfv0C3vY8mzvHzBDVM0x7UDhhLA=="
+  }
+
 }
 
-# provider "azurerm" {
-#   features {}
-# }
+provider "azurerm" {
+  features {}
+    # use_msi = true # managed_service_identity
 
+}
