3439223cbead6c0a0770f1c6163c3ea5d412003e
afaulkner <avfaulkner@gmail.com>
2021-07-07 15:58:26 -0700

    use cmsg pool as backend

--- app_gateway.tf
+++ app_gateway.tf
@@ -45,7 +45,7 @@ resource "azurerm_application_gateway" "appgateway" {
 
   backend_address_pool {
     name = local.backend_address_pool_name
-    ip_addresses = azurerm_linux_virtual_machine.broker[*].private_ip_address
+    ip_addresses = azurerm_linux_virtual_machine.gateway[*].private_ip_address
   }
 
   backend_http_settings {
