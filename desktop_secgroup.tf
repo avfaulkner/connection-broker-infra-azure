66d8278d4785cbe15921ebc549953a4a33460b76
afaulkner <avfaulkner@gmail.com>
2021-07-07 16:06:24 -0700

    modify broker, SGs

--- desktop_secgroup.tf
+++ desktop_secgroup.tf
@@ -19,7 +19,7 @@ resource "azurerm_network_security_rule" "desktop-rdp" {
   protocol                    = "Tcp"
   source_port_range           = "3389"
   destination_port_range      = "3389"
-  source_address_prefix       = azurerm_subnet.broker_subnet.address_prefix
+  source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.res_group.name
   network_security_group_name = azurerm_network_security_group.desktop-sg.name
@@ -50,7 +50,7 @@ resource "azurerm_network_security_rule" "desktop-pcoip" {
   protocol                    = "Tcp"
   source_port_range           = "4172"
   destination_port_range      = "4172"
-  source_address_prefix       = azurerm_subnet.broker_subnet.address_prefix
+  source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.res_group.name
   network_security_group_name = azurerm_network_security_group.desktop-sg.name
@@ -70,3 +70,10 @@ resource "azurerm_network_security_rule" "desktop-outbound" {
   resource_group_name         = azurerm_resource_group.res_group.name
   network_security_group_name = azurerm_network_security_group.desktop-sg.name
 }
+
+# Connect the security group to the subnet
+resource "azurerm_subnet_network_security_group_association" "desktop-sg-assoc" {
+  subnet_id                 = azurerm_subnet.desktop_subnet.id
+  network_security_group_id = azurerm_network_security_group.desktop-sg.id
+}
+
